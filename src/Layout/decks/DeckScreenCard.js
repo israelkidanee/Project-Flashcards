01aba4bc7a9e9c0e030c76436b4ad642d4251d5f
Israel Kidane <129226088+israelkidanee@users.noreply.github.com>
2023-08-07 06:27:18 -0400

    Update DeckScreenCard.js

--- src/Layout/decks/DeckScreenCard.js
+++ src/Layout/decks/DeckScreenCard.js
@@ -5,8 +5,6 @@ import { deleteCard } from "../../utils/api/index";
 function DeckScreenCard({ cards, deckId, url }) {
   const history = useHistory();
 
-  /* When the user clicks the "Delete" button associated with a card, this warning message will show.
-  If the user clicks "OK", the card is deleted using deleteCard() */
   const handleDeleteCardClick = (card) => {
     if (
       window.confirm("Delete this card? You will not be able to recover it.")
@@ -15,7 +13,6 @@ function DeckScreenCard({ cards, deckId, url }) {
     }
   };
 
-  // Creates a Bootstrap card for each card in the deck with an edit and delete button
   const cardDisplay = cards.map((card, index) => {
     return (
       <div className="deck-card card mt-2" key={index}>
@@ -48,8 +45,7 @@ function DeckScreenCard({ cards, deckId, url }) {
             className="btn btn-danger"
             onClick={() => handleDeleteCardClick(card)}
           >
-            {/* The anchor element will trigger the page to refresh after the card has been deleted.
-            This makes it so that the refreshed page will no longer display the deleted card. */}
+            {}
             <a href={url} className="text-white">
               <span className="oi oi-trash" />
             </a>
@@ -59,7 +55,6 @@ function DeckScreenCard({ cards, deckId, url }) {
     );
   });
 
-  // If there are cards in this deck, they will render. If not, the following message will return instead.
   if (cards.length) {
     return <div>{cardDisplay}</div>;
   } else {
